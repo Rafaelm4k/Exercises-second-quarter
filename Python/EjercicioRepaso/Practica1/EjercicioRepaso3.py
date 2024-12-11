2dee451babe6f290354a525d176ce12f1233f6d7
rafaeljr <rafaeldavidteran2019@gmail.com>
2024-12-11 12:03:13 +0100

    Ejercicios Practicables de python

---
+++ Python/EjercicioRepaso/Practica1/EjercicioRepaso3.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+
+if numero2 == 0:
+    print("El segundo numero es cero")
+else:
+    print(f"La division de los numeros es {numero1 / numero2}")
